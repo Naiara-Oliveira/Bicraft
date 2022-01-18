bf08b258117919bddc7abe4984f512fd2aff609d
Naiara-Oliveira <naiaraoliveira.9722@gmail.com>
2022-01-18 15:51:11 -0400

    otimizado

--- js/script.js
+++ js/script.js
@@ -20,3 +20,45 @@ function ativarProduto(parametro){
    console.log(elemento);
 }
 parametro.forEach(ativarProduto);
+//perguntas frequentes
+const perguntas = document.querySelectorAll(".perguntas button");
+
+function ativarPergunta(event){
+const pergunta = event.currentTarget;
+const controle = pergunta.getAttribute("aria-controls");
+const resposta = document.getElementById(controle);
+resposta.classList.toggle("ativa");
+
+const ativa = resposta.classList.contains("ativa");
+
+pergunta.setAttribute("aria-expanded" ,ativa);
+console.log(pergunta);
+}
+function eventosPerguntas(pergunta){
+   pergunta.addEventListener('click',ativarPergunta);
+ 
+ 
+}
+perguntas.forEach(eventosPerguntas);
+
+//galeria bicicletas
+const galeria = document.querySelectorAll(".bicicleta-imagem img");
+const galeriaContainer = document.querySelector(".bicicleta-imagem");
+
+function trocarImagens(event){
+const img = event.currentTarget;
+const media = matchMedia("(min-width:1000px)").matches;
+if(media){
+   galeriaContainer.prepend(img);
+}
+}
+
+function eventosGaleria(img){
+img.addEventListener('click', trocarImagens);
+}
+galeria.forEach(eventosGaleria);
+
+//Animacao
+if(window.SimpleAnime){
+new SimpleAnime();
+}
